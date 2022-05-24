2def1bdb0c2fb7d5a7adb5aa40820cf35b8f2954
Barimehdi77 <mbari@student.1337.ma>
2022-05-24 15:35:25 +0100

    create my first module

--- gettingStarted/myFirst.js
+++ gettingStarted/myFirst.js
@@ -1,6 +1,12 @@
 var http = require('http');
+var date = require('./myFirstModules');
+var url = require('url');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World! By Bari mehdi');
+  res.write("The Time is : " + date.myDateTime());
+  res.write("<br/>The Url path: " + req.url + "<br/>The url test: ");
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
 }).listen(8080);
