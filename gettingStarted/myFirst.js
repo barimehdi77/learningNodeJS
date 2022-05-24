94934a032cdd6ae0fff591e67be90b6c6c54d6c1
Barimehdi77 <mbari@student.1337.ma>
2022-05-24 16:22:53 +0100

    create file server

--- gettingStarted/myFirst.js
+++ gettingStarted/myFirst.js
@@ -1,12 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 var date = require('./myFirstModules');
 var url = require('url');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The Time is : " + date.myDateTime());
-  res.write("<br/>The Url path: " + req.url + "<br/>The url test: ");
-  var q = url.parse(req.url, true).query;
-  var txt = q.year + " " + q.month;
-  res.end(txt);
+http.createServer(function(req, res){
+	var q = url.parse(req.url, true);
+	var fileName = "." + q.pathname;
+	if (fileName === "./")
+		fileName = "./index.html";
+	console.log(fileName);
+	fs.readFile(fileName, function(err, data) {
+		if (err) {
+			res.writeHead(404, {'content-Tyoe': 'text/html'});
+			return (res.end("404 Not Found"));
+		}
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		return res.end();
+	});
 }).listen(8080);
