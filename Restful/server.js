9c513cd44dab24892c38d04e00031697643ae62c
Barimehdi77 <mbari@student.1337.ma>
2022-05-26 23:25:08 +0100

    create post requist to add data and list all data

--- Restful/server.js
+++ Restful/server.js
@@ -1,29 +1,44 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var bodyParser = require("body-parser");
-
-app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-app.use(express.urlencoded({ extended: true }));
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var i = 4;
 
 var user = {
-	"user4" : {
-	   "name" : "mounir",
-	   "password" : "password4",
-	   "profession" : "teacher",
-	   "id": 4
+		"user4" : {
+		"name" : "mounir",
+		"password" : "password4",
+		"profession" : "teacher",
+		"id": 4
 	}
 };
 
+app.get('/', function (req, res) {
+	fs.readFile( __dirname + "/" + "index.html" , function (err, data) {
+		res.end(data);
+	});
+});
+
 app.post('/adduser', function (req, res) {
+	console.log(req.body);
+	var filename = __dirname + "/" + "users.json";
 	// First read existing users.
-	fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-	   data = JSON.parse( data );
-	   data["user4"] = user["user4"];
-	   console.log( data );
-	   res.end(JSON.stringify(data) );
+	fs.readFile( filename, 'utf8', function (err, data) {
+		data = JSON.parse( data );
+		var user = req.body;
+		user["id"] = i++;
+		data["user4"] = req.body;
+		console.log( data );
+		filedata = data;
+		fs.writeFile(filename, JSON.stringify(data), function (err) {
+			if (err)
+				console.log(err);
+			else
+				console.log("data added");
+		});
+		res.end(JSON.stringify(data));
 	});
 });
 
@@ -41,3 +56,6 @@ var server = app.listen(8080, function () {
 	var port = server.address().port;
 	console.log("listening at http://%s:%s", "0.0.0.0", port)
 });
+
+
+
