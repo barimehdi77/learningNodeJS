df5e3c52781caf0cbf59dad678f4db5b99e4a236
Barimehdi77 <mbari@student.1337.ma>
2022-05-29 17:11:05 +0100

    create teachers class

--- nest-project/src/app/app.module.ts
+++ nest-project/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StudentController } from '../student/student.controller';
+import { TeachersController } from '../teacher/teacher.controller';
 
 @Module({
   imports: [],
-  controllers: [ StudentController ]
+  controllers: [ StudentController, TeachersController ]
 })
 export class AppModule {}
---
+++ nest-project/src/teacher/teacher.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Put } from "@nestjs/common";
+
+
+@Controller('teachers')
+export class TeachersController {
+	@Get()
+	getTeachers() {
+		return ("Get All Teachers");
+	};
+
+	@Get('/:teacherId')
+	getTeacherById() {
+		return ("Get Teacher By Id")
+	}
+
+	@Get('/:teacherId/students')
+	getAllteacherstudents() {
+		return ("All Studnet's of this teacher");
+	}
+
+	@Put('/:teacherid:students/:studentid')
+	updateStudentByTeacherId() {
+		return ("studnet info updated");
+	}
+}
