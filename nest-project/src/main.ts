97498b5271b5e50d79d437f7822d7a60cf85aa56
barimehdi77 <mbari@student.1337.ma>
2022-06-21 07:08:13 -0400

    create a new branch for myReactApp

--- nest-project/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
-import { StudentController } from "./student/student.controller";
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
-}
-bootstrap();
--- nest-project/src/teacher/dto/teacher.dto.ts
+++
@@ -1,9 +0,0 @@
-export class FindTeacherResponseDto {
-	id: string;
-	name: string;
-}
-
-export class TeacherResponseDto {
-	id: string;
-	name: string;
-}
