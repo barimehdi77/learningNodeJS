034e7de2a0dc6edbdd9d4389dbbddf1b0f903a3b
barimehdi77 <mbari@student.1337.ma>
2022-06-21 09:58:28 -0400

    simple edit

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,15 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "reactamplifytestapp": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string"
+        }
+    },
+    "analytics": {
+        "reactamplifytestapp": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    }
+}
